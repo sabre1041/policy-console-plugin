36d09db7d1ee05ac14e878fcf81db5eeb9723dd4
Andrew Block <andy.block@gmail.com>
2022-08-14 19:42:19 -0500

    Updates for i18n

---
+++ i18next-parser.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    defaultNamespace: 'plugin__policy-console-plugin',
+    createOldCatalogs: false,
+    keySeparator: false,
+    locales: ['en'],
+    namespaceSeparator: '~',
+    reactNamespace: false,
+    useKeysAsDefaultValue: true,
+};
